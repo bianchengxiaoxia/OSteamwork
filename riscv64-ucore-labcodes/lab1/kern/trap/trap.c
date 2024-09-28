aa2f70a9e4c00a4f427dbd26b42562e5b967b13f
haoyue <121660231+haoyueliuzhao@users.noreply.github.com>
2024-09-28 16:46:33 +0800

    完善trap.c函数中处理时钟中断的部分，第一次提交

--- riscv64-ucore-labcodes/lab1/kern/trap/trap.c
+++ riscv64-ucore-labcodes/lab1/kern/trap/trap.c
@@ -112,6 +112,18 @@ void interrupt_handler(struct trapframe *tf) {
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+            clock_set_next_event();
+            ticks++;
+            if(ticks==100)
+            {
+                print_ticks();
+                num++;
+            }
+            if(num==10)
+            {
+                shutdown();
+
+            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
