90021de40f0b1c381623aaaee88951727d2a052a
bianchengxiaoxia <2981411754@qq.com>
2024-10-26 15:02:05 +0800

    best_fit算法分析

--- riscv64-ucore-labcodes/lab2/kern/mm/best_fit_pmm.c
+++ riscv64-ucore-labcodes/lab2/kern/mm/best_fit_pmm.c
@@ -55,7 +55,7 @@
  *               (5.2) reset the fields of pages, such as p->ref, p->flags (PageProperty)
  *               (5.3) try to merge low addr or high addr blocks. Notice: should change some pages's p->property correctly.
  */
-free_area_t free_area;
+static free_area_t free_area;
 
 #define free_list (free_area.free_list)
 #define nr_free (free_area.nr_free)
@@ -72,8 +72,9 @@ best_fit_init_memmap(struct Page *base, size_t n) {
     struct Page *p = base;
     for (; p != base + n; p ++) {
         assert(PageReserved(p));
-
-        /*LAB2 EXERCISE 2: YOUR CODE*/ 
+        p->flags = p->property = 0;
+        set_page_ref(p, 0);
+        /*LAB2 EXERCISE 2: 2212437*/ 
         // 清空当前页框的标志和属性信息，并将页框的引用计数设置为0
     }
     base->property = n;
@@ -85,10 +86,16 @@ best_fit_init_memmap(struct Page *base, size_t n) {
         list_entry_t* le = &free_list;
         while ((le = list_next(le)) != &free_list) {
             struct Page* page = le2page(le, page_link);
-             /*LAB2 EXERCISE 2: YOUR CODE*/ 
+             /*LAB2 EXERCISE 2: 2212437*/ 
             // 编写代码
             // 1、当base < page时，找到第一个大于base的页，将base插入到它前面，并退出循环
             // 2、当list_next(le) == &free_list时，若已经到达链表结尾，将base插入到链表尾部
+            if (base < page) {
+                list_add_before(le, &(base->page_link));
+                break;
+            } else if (list_next(le) == &free_list) {
+                list_add(le, &(base->page_link));
+            }
         }
     }
 }
@@ -102,27 +109,29 @@ best_fit_alloc_pages(size_t n) {
     struct Page *page = NULL;
     list_entry_t *le = &free_list;
     size_t min_size = nr_free + 1;
-     /*LAB2 EXERCISE 2: YOUR CODE*/ 
+     /*LAB2 EXERCISE 2: 2212437*/ 
     // 下面的代码是first-fit的部分代码，请修改下面的代码改为best-fit
     // 遍历空闲链表，查找满足需求的空闲页框
     // 如果找到满足需求的页面，记录该页面以及当前找到的最小连续空闲页框数量
     while ((le = list_next(le)) != &free_list) {
         struct Page *p = le2page(le, page_link);
-        if (p->property >= n) {
+        if (p->property >= n && p->property < min_size) {
             page = p;
-            break;
+            min_size = p->property;
         }
     }
 
     if (page != NULL) {
-        list_entry_t* prev = list_prev(&(page->page_link));
+        list_entry_t *prev = list_prev(&(page->page_link));
         list_del(&(page->page_link));
+
         if (page->property > n) {
-            struct Page *p = page + n;
-            p->property = page->property - n;
-            SetPageProperty(p);
-            list_add(prev, &(p->page_link));
+            struct Page *remaining = page + n;
+            remaining->property = page->property - n;
+            SetPageProperty(remaining);
+            list_add(prev, &(remaining->page_link));
         }
+
         nr_free -= n;
         ClearPageProperty(page);
     }
@@ -138,8 +147,11 @@ best_fit_free_pages(struct Page *base, size_t n) {
         p->flags = 0;
         set_page_ref(p, 0);
     }
-    /*LAB2 EXERCISE 2: YOUR CODE*/ 
+    /*LAB2 EXERCISE 2: 2212437*/ 
     // 编写代码
+    base->property = n;
+    SetPageProperty(base);
+    nr_free += n;
     // 具体来说就是设置当前页块的属性为释放的页块数、并将当前页块标记为已分配状态、最后增加nr_free的值
 
     if (list_empty(&free_list)) {
@@ -160,8 +172,14 @@ best_fit_free_pages(struct Page *base, size_t n) {
     list_entry_t* le = list_prev(&(base->page_link));
     if (le != &free_list) {
         p = le2page(le, page_link);
-        /*LAB2 EXERCISE 2: YOUR CODE*/ 
+        /*LAB2 EXERCISE 2: 2212437*/ 
          // 编写代码
+        if (p + p->property == base) {
+            p->property += base->property;
+            ClearPageProperty(base);
+            list_del(&(base->page_link));
+            base = p;
+        }
         // 1、判断前面的空闲页块是否与当前页块是连续的，如果是连续的，则将当前页块合并到前面的空闲页块中
         // 2、首先更新前一个空闲页块的大小，加上当前页块的大小
         // 3、清除当前页块的属性标记，表示不再是空闲页块
@@ -327,4 +345,6 @@ const struct pmm_manager best_fit_pmm_manager = {
     .nr_free_pages = best_fit_nr_free_pages,
     .check = best_fit_check,
 };
+
+
 
